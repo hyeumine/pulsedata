f286b8b8822c2a623b252f6c1271ce4ca7547c36
hyeumine <busicoeugene@gmail.com>
2020-07-07 20:34:25 +0800

    Update incomingsms.php

---
+++ smsrestcalls/incomingsms.php
@@ -0,0 +1,12 @@
+<?php
+
+$p =  mysqli_connect("us-cdbr-east-02.cleardb.com","b01244ab209a87","2ad7c806","heroku_3223da8055cbdc3");
+$json = file_get_contents('php://input');
+$json = stripslashes($json);
+$values = json_decode($json, true);
+$arr = $values['inboundSMSMessageList']['inboundSMSMessage'];
+foreach($arr as $a){
+	mysqli_query($p, "INSERT INTO receivedsms VALUES(NULL,'".$a['messageId']."','".$a['senderAddress']."','".$a['message']."','".$a['dateTime']."')");
+}
+
+?>
