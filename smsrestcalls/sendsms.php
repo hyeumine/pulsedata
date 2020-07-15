3d4260341b9a553d1b756e4e510114e8ceb80c2e
hyeumine <busicoeugene@gmail.com>
2020-07-15 23:31:33 +0800

    Update sendsms.php

--- smsrestcalls/sendsms.php
+++ smsrestcalls/sendsms.php
@@ -13,7 +13,7 @@
 		    	 "clientCorrelator"=> "123456",
 				   "senderAddress"=> "21589965",
 				   "outboundSMSTextMessage"=> array("message"=>$_REQUEST['message']),
-   					"address"=> $token[1]
+   					"address"=> '9664136950'
 		    )
 		);
 
