850372fa88e2a619f9cc95cd449d22efccd95d4c
hyeumine <busicoeugene@gmail.com>
2020-07-15 23:26:46 +0800

    Update sendsms.php

--- smsrestcalls/sendsms.php
+++ smsrestcalls/sendsms.php
@@ -3,11 +3,32 @@
 	$res = mysqli_query($p, "SELECT * FROM subscriber as s JOIN qperson as p ON s.subscriber_number=p.mobile_number");
 
 ?>
+<?php
+	if(isset($_REQUEST['message']) && isset($_REQUEST['token'])){
+		$token = explode('-',$_REQUEST['token']);
+		$postRequest = array(
+		    'outboundSMSMessageRequest' =>
+		    array(
+		    	 "clientCorrelator"=> "123456",
+				   "senderAddress"=> "21589965",
+				   "outboundSMSTextMessage"=> array("message"=>$_REQUEST['message']),
+   					"address"=> $token[1]
+		    )
+		);
+
+		$cURLConnection = curl_init('https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/21589965/requests?access_token='.$token[0]);
+		curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $postRequest);
+		curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+		$apiResponse = curl_exec($cURLConnection);
+		curl_close($cURLConnection);
+	}
+?>
 <form action='' method='post'>
-<select>
+<select name='token'>
 <?php
 	while($r = mysqli_fetch_assoc($res)){
-		echo "<option value='". $r['access_token'] ."'>". $r['qcode'] . " - ". $r['subscriber_number']."</option>";
+		echo "<option value='". $r['access_token']. "-" . $r['subscriber_number'] ."'>". $r['qcode'] . " - ". $r['subscriber_number']."</option>";
 	}
 
 ?>
