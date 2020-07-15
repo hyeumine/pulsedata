daf8e4c3e4043df37cb04e0670448fd3cc1eeb2c
hyeumine <busicoeugene@gmail.com>
2020-07-15 23:05:33 +0800

    Update sendsms.php

--- smsrestcalls/sendsms.php
+++ smsrestcalls/sendsms.php
@@ -1,6 +1,9 @@
 <?php
 	 $p =  mysqli_connect("us-cdbr-east-02.cleardb.com","b01244ab209a87","2ad7c806","heroku_3223da8055cbdc3");
-	mysqli_query($p, "SELECT * FROM subscriber as s JOIN qperson as p ON s.subscriber_number=p.mobile_number");
-	echo 1;
+	$res = mysqli_query($p, "SELECT * FROM subscriber as s JOIN qperson as p ON s.subscriber_number=p.mobile_number");
 
-?>dsfdsf
+	while($r = mysql_fetch_assoc($res)){
+		var_dump($r);
+	}
+
+?>
