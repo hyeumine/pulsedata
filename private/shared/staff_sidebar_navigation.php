43d94f744f9cadf5358927e57269431168dd3515
paulquickstart <dongallo.paul@gmail.com>
2020-07-17 01:08:28 +0800

    link back to dashboard

--- private/shared/staff_sidebar_navigation.php
+++ private/shared/staff_sidebar_navigation.php
@@ -13,7 +13,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="<?php url_for('/staff/patients/dashboard.php'); ?>">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
