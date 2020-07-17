331144363f0516c929b9d0ec3d3b16d3f981e114
paulquickstart <dongallo.paul@gmail.com>
2020-07-18 01:14:27 +0800

    debug 114

--- private/initialize.php
+++ private/initialize.php
@@ -1,35 +1,5 @@
 <?php 
 
-define("DB_SERVER", "us-cdbr-east-02.cleardb.com");
-define("DB_USER", "b01244ab209a87");
-define("DB_PASS", "2ad7c806");
-define("DB_NAME", "heroku_3223da8055cbdc3");
-
-// Connecting to and selecting a MySQL database named sakila
-// Hostname: 127.0.0.1, username: your_user, password: your_pass, db: sakila
-$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-
-// Oh no! A connect_errno exists so the connection attempt failed!
-if ($mysqli->connect_errno) {
-    // The connection failed. What do you want to do? 
-    // You could contact yourself (email?), log the error, show a nice page, etc.
-    // You do not want to reveal sensitive information
-
-    // Let's try this:
-    echo "Sorry, this website is experiencing problems.";
-
-    // Something you should not do on a public site, but this example will show you
-    // anyways, is print out MySQL error related information -- you might log this
-    echo "Error: Failed to make a MySQL connection, here is why: \n";
-    echo "Errno: " . $mysqli->connect_errno . "\n";
-    echo "Error: " . $mysqli->connect_error . "\n";
-    
-    // You might want to show them something nice, but we will simply exit
-    exit;
-}
-
-exit();
-
 define("SITE_NAME", $_SERVER['SERVER_NAME'] );
 
 date_default_timezone_set('Asia/Manila');
