f8f9fb6737488c9249fb850f6507e27c3a878f14
paulquickstart <dongallo.paul@gmail.com>
2020-07-17 21:34:04 +0800

    debug 717

--- private/initialize.php
+++ private/initialize.php
@@ -1,7 +1,7 @@
 <?php 
 define("SITE_NAME", $_SERVER['SERVER_NAME'] );
 
-// date_default_timezone_set('Asia/Manila');
+date_default_timezone_set('Asia/Manila');
 
 // dirname() returns the path to the parent directory
 define("PRIVATE_PATH", dirname(__FILE__));
@@ -13,5 +13,27 @@ $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
 
+
+
 require_once(PRIVATE_PATH.'/functions.php');
 
+echo "teseting here";
+exit();
+
+require_once('db_credentials.php');
+require_once('database_functions.php');
+
+foreach(glob('classes/*.class.php') as $file) {
+	require_once($file);
+}
+
+// Autoload class definitions
+function my_autoload($class) {
+	if(preg_match('/\A\w+\Z/', $class)) {
+	  include('classes/' . $class . '.class.php');
+	}
+}
+spl_autoload_register('my_autoload');
+
+$database = db_connect();
+DatabaseObject::set_database($database);
