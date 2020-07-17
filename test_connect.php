6b66736041da12b49345a37c046f1e2b0ff05b06
paulquickstart <dongallo.paul@gmail.com>
2020-07-18 01:47:01 +0800

    test 4

--- test_connect.php
+++ test_connect.php
@@ -1,7 +1,7 @@
 <?php
 
 define("DB_SERVER", "us-cdbr-east-02.cleardb.com");
-define("DB_USER", "b01244ab209a87 sdfsdfsdf");
+define("DB_USER", "b01244ab209a87");
 define("DB_PASS", "2ad7c806");
 define("DB_NAME", "heroku_3223da8055cbdc3");
 
@@ -9,8 +9,8 @@ define("DB_NAME", "heroku_3223da8055cbdc3");
 $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
 // Check connection
-if ($mysqli ->connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli ->connect_error;
+if ($mysqli->connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli->connect_error;
   exit();
 }else{
 	echo "connect";
