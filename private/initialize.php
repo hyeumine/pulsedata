8afc05eba2f2d6da360d7ff233acfacf3fe5ac52
paulquickstart <dongallo.paul@gmail.com>
2020-07-17 21:37:38 +0800

    debug937

--- private/initialize.php
+++ private/initialize.php
@@ -13,14 +13,11 @@ $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
 
-
-
 require_once(PRIVATE_PATH.'/functions.php');
 
+require_once('db_credentials.php');
 echo "teseting here";
 exit();
-
-require_once('db_credentials.php');
 require_once('database_functions.php');
 
 foreach(glob('classes/*.class.php') as $file) {
