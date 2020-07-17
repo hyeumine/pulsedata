5f76153f40e8f9606787ade729dd876ef9f9c937
paulquickstart <dongallo.paul@gmail.com>
2020-07-17 21:10:23 +0800

    finding nimo 1 test

--- private/initialize.php
+++ private/initialize.php
@@ -13,8 +13,6 @@ $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
 
-
-
 require_once(PRIVATE_PATH.'/functions.php');
 require_once('db_credentials.php');
 require_once('database_functions.php');
