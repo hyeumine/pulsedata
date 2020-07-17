d6b0ecef1eb7dacf3807bc85f94640b489f28cce
paulquickstart <dongallo.paul@gmail.com>
2020-07-18 01:45:16 +0800

    teset connect 3

--- test_connect.php
+++ test_connect.php
@@ -1,7 +1,7 @@
 <?php
 
 define("DB_SERVER", "us-cdbr-east-02.cleardb.com");
-define("DB_USER", "b01244ab209a87");
+define("DB_USER", "b01244ab209a87 sdfsdfsdf");
 define("DB_PASS", "2ad7c806");
 define("DB_NAME", "heroku_3223da8055cbdc3");
 
