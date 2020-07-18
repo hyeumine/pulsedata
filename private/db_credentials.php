e1889edca73060ea5865ec8d7964ef52801df037
paulquickstart <dongallo.paul@gmail.com>
2020-07-18 09:25:46 +0800

    db comment

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DB_SERVER", "us-cdbr-east-02.cleardb.com");
-define("DB_USER", "b01244ab209a87");
-define("DB_PASS", "2ad7c806");
-define("DB_NAME", "heroku_3223da8055cbdc3");
+// define("DB_SERVER", "us-cdbr-east-02.cleardb.com");
+// define("DB_USER", "b01244ab209a87");
+// define("DB_PASS", "2ad7c806");
+// define("DB_NAME", "heroku_3223da8055cbdc3");
 
 // define("DB_SERVER", "localhost");
 // define("DB_USER", "root");
