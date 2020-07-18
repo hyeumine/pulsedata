cf3ce84cc8a9cf2b8104a59811148cc36cf052ab
paulquickstart <dongallo.paul@gmail.com>
2020-07-18 09:18:07 +0800

    display error

--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,9 @@
 <?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define("SITE_NAME", $_SERVER['SERVER_NAME'] );
 
 date_default_timezone_set('Asia/Manila');
