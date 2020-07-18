a6dbaec012311970b806b7a832189a74f60a4e5d
paulquickstart <dongallo.paul@gmail.com>
2020-07-18 16:08:12 +0800

    js footer for dashboard

--- private/shared/staff_javascript_top_footer.php
+++ private/shared/staff_javascript_top_footer.php
@@ -14,38 +14,48 @@
  if( is_page( current_page(), SITE_NAME."/public/staff/patients/") ){
  ?>	
 
-   <!-- show only data tables -->
-<!-- 	<script src="<?php // html(url_for("/assets/js/moment/min/moment.min.js")); ?>"></script>
-
-	<script src="<?php // html(url_for("/assets/js/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js")); ?>"></script> -->
-
-   <!-- Page level custom scripts -->
+  <!-- Page level custom scripts -->
   <!-- Page level plugins -->
   <script src="<?php html(url_for("/assets/vendor/datatables/jquery.dataTables.min.js")); ?>"></script>
 
-  <script src="<?php html(url_for("/assets/vendor/datatables/dataTables.bootstrap4.min.js")) ;?>"></script>
+  <script src="<?php html(url_for("/assets/vendor/datatables/dataTables.bootstrap4.min.js")); ?>"></script>
 
   <!-- Page level custom scripts -->
   <script src="<?php html(url_for("/assets/js/demo/datatables-demo.js")); ?>"></script>
 
-   <script>
 
-   	// A $( document ).ready() block.
-	// $( document ).ready(function() {
-	   	
-	//    	$('#datetimepicker1').datetimepicker({
-	//    		format: 'L'
-	//    	});
+<?php
+ }elseif( is_page( current_page(), SITE_NAME."/public/staff/patients/dashboard.php") ){  ?>
 
-	//    	$('#datetimepicker2').datetimepicker({
-	//    		format: 'L'
-	//    	});
+	<!-- Page level plugins -->
+  	<script src="<?php html(url_for("/assets/vendor/chart.js/Chart.min.js")); ?>"></script>
 
-	   	
-	// });
+  	<!-- Page level custom scripts -->
+  	<script src="<?php html(url_for('/assets/js/demo/chart-area-demo.js')); ?>"></script>
 
-   </script>
+  	<script src="<?php html(url_for('/assets/js/demo/chart-pie-demo.js')); ?>"></script>
 
+ 	<!-- show only data tables -->
+	<script src="<?php html(url_for("/assets/js/moment/min/moment.min.js")); ?>"></script>
+
+	<script src="<?php html(url_for("/assets/js/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js")); ?>"></script>
+
+	   <script>
+
+		$( document ).ready(function() {
+		   	
+		   	$('#datetimepicker1').datetimepicker({
+		   		format: 'L'
+		   	});
+
+		   	$('#datetimepicker2').datetimepicker({
+		   		format: 'L'
+		   	});
+
+		   	
+		});
+
+   		</script>
 <?php
  }
 ?>
