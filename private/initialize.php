b8a01473b7516a172db3d744942854caa4912ec5
paulquickstart <dongallo.paul@gmail.com>
2020-07-18 09:20:41 +0800

    classes

--- private/initialize.php
+++ private/initialize.php
@@ -13,7 +13,7 @@ define("PRIVATE_PATH", dirname(__FILE__));
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
 define("PUBIC_PATH", PROJECT_PATH . '/public');
 define("SHARED_PATH", PRIVATE_PATH . '/shared');
-define("CLASS_PATH", PRIVATE_PATH . '/class');
+define("CLASS_PATH", PRIVATE_PATH . '/classes');
 
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
