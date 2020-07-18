b5ac0a8f39ba6f74c3bab713539d3d98e230563f
paulquickstart <dongallo.paul@gmail.com>
2020-07-18 09:29:05 +0800

    properties

--- private/initialize.php
+++ private/initialize.php
@@ -23,10 +23,10 @@ require_once(PRIVATE_PATH.'/functions.php');
 // require_once('db_credentials.php');
 
 
-define("DB_SERVER", "us-cdbr-east-02.cleardb.com");
-define("DB_USER", "b01244ab209a87");
-define("DB_PASS", "2ad7c806");
-define("DB_NAME", "heroku_3223da8055cbdc3");
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "pulsedata");
 
 
 function db_connect() {
