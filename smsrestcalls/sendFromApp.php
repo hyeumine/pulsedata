43453e00c2dbec8c518565fa25eac82cefd422d0
hyeumine <busicoeugene@gmail.com>
2020-07-27 22:46:21 +0800

    Update sendFromApp.php

--- smsrestcalls/sendFromApp.php
+++ smsrestcalls/sendFromApp.php
@@ -1,6 +1,6 @@
 <?php
  	$p =  mysqli_connect("us-cdbr-east-02.cleardb.com","b01244ab209a87","2ad7c806","heroku_3223da8055cbdc3");
-	$res = mysqli_query($p, "SELECT * FROM subscriber as s JOIN qperson as p ON s.subscriber_number=p.mobile_number WHERE s.subscriber_number='". $_REQUEST['mobile_number'] ."'");
+	$res = mysqli_query($p, "SELECT p.* FROM subscriber as s JOIN qperson as p ON s.subscriber_number=p.mobile_number WHERE s.subscriber_number='". $_REQUEST['mobile_number'] ."'");
 	$b = 0;
 	while($r = mysqli_fetch_assoc($res)){
 			$b=1;
@@ -8,6 +8,7 @@
 				echo  2;
 			}
 			else{
+				mysqli_query($p, "INSERT INTO qperson_summary VALUES(NULL, "'. $r['id'] .'", ".$_REQUEST['type'].",'". $_REQUEST['value'] ."',NULL,1)");
 				echo 0;
 			}
 			break;
