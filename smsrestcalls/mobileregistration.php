0e7bb149967d3b242df6479e9d768a5cf97052b6
hyeumine <busicoeugene@gmail.com>
2020-08-02 23:50:05 +0800

    Update mobileregistration.php

--- smsrestcalls/mobileregistration.php
+++ smsrestcalls/mobileregistration.php
@@ -1,7 +1,7 @@
 <?php
 	 $p =  mysqli_connect("us-cdbr-east-02.cleardb.com","b01244ab209a87","2ad7c806","heroku_3223da8055cbdc3");
 
-	 $res = mysqli_query($p, "SELECT MAX(id) as c FROM qperson WHERE lgu_code='MOB-001");
+	 $res = mysqli_query($p, "SELECT MAX(id) as c FROM qperson WHERE lgu_code='MOB-001'");
 
 	 $c = 0;
 	 while($r = mysqli_fetch_assoc($res)){
