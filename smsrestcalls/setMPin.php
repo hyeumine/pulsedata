d735c252db5c178f8037fe57cd88b28f2f754351
hyeumine <busicoeugene@gmail.com>
2020-08-03 09:52:36 +0800

    Create setMPin.php

---
+++ smsrestcalls/setMPin.php
@@ -0,0 +1,16 @@
+<?php
+	 $p =  mysqli_connect("us-cdbr-east-02.cleardb.com","b01244ab209a87","2ad7c806","heroku_3223da8055cbdc3");
+
+
+	 $res = mysqli_query($p, "SELECT * FROM qperson WHERE qcode='".$_REQUEST['qcode']."'");
+
+	    $b = 0;
+		while($r = mysqli_fetch_assoc($res)){
+			$b=1;
+			echo 0;
+			break;
+		}
+		if($b==0)
+		   echo 1;
+
+?>
