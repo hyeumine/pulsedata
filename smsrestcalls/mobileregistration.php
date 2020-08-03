132d7dd741e40786f7c8fcdb5218dd0cb00dbb70
hyeumine <busicoeugene@gmail.com>
2020-08-03 11:50:09 +0800

    Update

--- smsrestcalls/mobileregistration.php
+++ smsrestcalls/mobileregistration.php
@@ -10,6 +10,6 @@
 	 }
 
 	 $res = mysqli_query($p, "INSERT INTO qperson VALUES(NULL,'MOB-". str_pad($c+1, 5, '0', STR_PAD_LEFT) ."','MOB-001','".$_REQUEST['fname']."','".$_REQUEST['mname']."','".$_REQUEST['lname']."','".$_REQUEST['mobile_number']."','".$_REQUEST['address']."','','',NULL)");
-	 echo "MOB-". str_pad($c, 5, '0', STR_PAD_LEFT);
+	 echo "MOB-". str_pad($c+1, 5, '0', STR_PAD_LEFT);
 
 ?>
